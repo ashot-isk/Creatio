5070938cadda9896e1197e0c3cb4c28c0964786a
Ashot.Iskandaryan <Ashot.Iskandaryan@ardshinbamk.am>
2023-08-23 11:34:37 +0400

    added new object and changed view

--- UsrRealityFreedomUI/Schemas/UsrRealityFreedomUI_FormPage/UsrRealityFreedomUI_FormPage.js
+++ UsrRealityFreedomUI/Schemas/UsrRealityFreedomUI_FormPage/UsrRealityFreedomUI_FormPage.js
@@ -2,6 +2,29 @@ define("UsrRealityFreedomUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, funct
 	return {
 		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
 			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"dataSourceName": "PDS",
+					"entitySchemaName": "UsrRealityFreedomUI"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"columns": [
+						{
+							"id": "55936210-aaf4-407d-bc91-8abad94a5a07",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					]
+				}
+			},
+			{
 				"operation": "insert",
 				"name": "UsrName",
 				"values": {
@@ -21,62 +44,157 @@ define("UsrRealityFreedomUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, funct
 				"index": 0
 			},
 			{
-				"operation": "merge",
-				"name": "AttachmentList",
+				"operation": "insert",
+				"name": "NumberInput_avu4th7",
 				"values": {
-					"type": "crt.FileList",
-					"masterRecordColumnValue": "$Id",
-					"recordColumnName": "RecordId",
 					"layoutConfig": {
-						"colSpan": 2,
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.NumberAttribute_3a4sh3d",
+					"labelPosition": "auto",
+					"control": "$NumberAttribute_3a4sh3d"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "NumberInput_y8fewe4",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.NumberAttribute_z3ys0gd",
+					"labelPosition": "auto",
+					"control": "$NumberAttribute_z3ys0gd"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "ComboBox_ht22sdw",
+				"values": {
+					"layoutConfig": {
 						"column": 1,
 						"row": 1,
-						"rowSpan": 6
+						"colSpan": 1,
+						"rowSpan": 1
 					},
-					"items": "$AttachmentList",
-					"primaryColumnName": "AttachmentListDS_Id",
-					"columns": [
-						{
-							"id": "55936210-aaf4-407d-bc91-8abad94a5a07",
-							"code": "AttachmentListDS_Name",
-							"caption": "#ResourceString(AttachmentListDS_Name)#",
-							"dataValueType": 28,
-							"width": 200
-						}
-					],
-					"viewType": "gallery",
-					"tileSize": "small"
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_to3p4h7",
+					"labelPosition": "auto",
+					"control": "$LookupAttribute_to3p4h7",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
 				},
-				"parentName": "AttachmentsTabContainer",
+				"parentName": "GeneralInfoTabContainer",
 				"propertyName": "items",
 				"index": 0
 			},
 			{
-				"operation": "merge",
-				"name": "Feed",
+				"operation": "insert",
+				"name": "ComboBox_mjfhckp",
 				"values": {
-					"type": "crt.Feed",
-					"feedType": "Record",
-					"primaryColumnValue": "$Id",
-					"cardState": "$CardState",
-					"dataSourceName": "PDS",
-					"entitySchemaName": "UsrRealityFreedomUI"
+					"layoutConfig": {
+						"column": 2,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_8oqhdj4",
+					"labelPosition": "auto",
+					"control": "$LookupAttribute_8oqhdj4",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
 				},
-				"parentName": "FeedTabContainer",
+				"parentName": "GeneralInfoTabContainer",
 				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_yu12wbe",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_yu12wbe_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "ComboBox_mjfhckp",
+				"propertyName": "listActions",
 				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Input_bmuwoba",
+				"values": {
+					"type": "crt.Input",
+					"label": "$Resources.Strings.StringAttribute_oin9asi",
+					"labelPosition": "auto",
+					"control": "$StringAttribute_oin9asi",
+					"multiline": false
+				},
+				"parentName": "GeneralInfoTab",
+				"propertyName": "items",
+				"index": 1
 			}
 		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
 		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
 			"attributes": {
+				"Id": {
+					"modelConfig": {
+						"path": "PDS.Id"
+					}
+				},
 				"UsrName": {
 					"modelConfig": {
 						"path": "PDS.UsrName"
 					}
 				},
-				"Id": {
+				"NumberAttribute_3a4sh3d": {
 					"modelConfig": {
-						"path": "PDS.Id"
+						"path": "PDS.UsrPriceUSD"
+					}
+				},
+				"NumberAttribute_z3ys0gd": {
+					"modelConfig": {
+						"path": "PDS.UsrArea"
+					}
+				},
+				"LookupAttribute_to3p4h7": {
+					"modelConfig": {
+						"path": "PDS.UsrType"
+					}
+				},
+				"LookupAttribute_8oqhdj4": {
+					"modelConfig": {
+						"path": "PDS.UsrOfferType"
+					}
+				},
+				"StringAttribute_oin9asi": {
+					"modelConfig": {
+						"path": "PDS.UsrComment"
 					}
 				}
 			}
@@ -89,18 +207,6 @@ define("UsrRealityFreedomUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, funct
 						"entitySchemaName": "UsrRealityFreedomUI"
 					},
 					"scope": "page"
-				},
-				"AttachmentListDS": {
-					"type": "crt.EntityDataSource",
-					"scope": "viewElement",
-					"config": {
-						"entitySchemaName": "SysFile",
-						"attributes": {
-							"Name": {
-								"path": "Name"
-							}
-						}
-					}
 				}
 			},
 			"primaryDataSourceName": "PDS"
